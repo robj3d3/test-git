eb76234391432f4c6fc2edc58edccfcbdd2bfc8e
RobH18 <roboffice100@gmail.com>
2021-02-13 17:55:57 +0000

    Cool feature finalised

--- coolfeature.py
+++ coolfeature.py
@@ -3,4 +3,8 @@ import random
 def cool_feature():
     return random.randint(1,10)
 
+def get_epicfeature():
+    return "This is an epic feature!"
+
 print(cool_feature())
+print(get_epicfeature())
--- workshop.py
+++ workshop.py
@@ -1,3 +1,8 @@
-print("Hello World")
+import coolfeature
+
 def main():
   print("This function was added from GitHub")
+  print(coolfeature.cool_feature())
+  print(coolfeature.get_epicfeature())
+
+main()
