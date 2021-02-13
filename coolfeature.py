f9f42fd92243b4e8b092173a6cc3034a3b81108f
RobH18 <roboffice100@gmail.com>
2021-02-13 17:22:26 +0000

    Add function to generate random numbers

--- coolfeature.py
+++ coolfeature.py
@@ -1,5 +1,12 @@
+import random
+
+def cool_feature():
+    return random.randint(1,10)
+
 def confusing():
     return "Confusing Git is great fun!"
 
 def giraffe():
     return "Not really sure why this is here"
+
+print(cool_feature())
